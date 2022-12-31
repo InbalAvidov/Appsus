4ee88aa449ddef304b4b2870a115be8f6281c4d5
HazanOmri <omrihazan1313@gmail.com>
2022-12-31 19:24:20 +0200

    Nav-bar redesign

--- cmps/app-header.jsx
+++ cmps/app-header.jsx
@@ -2,12 +2,15 @@ const { Link, NavLink } = ReactRouterDOM
 
 export function AppHeader() {
 
+
+
+
     return <header className="app-header full main-layout">
         <Link to="/">
             <h1 className="logo">AppSus</h1>
         </Link>
-        <div className="main-menu">
-            <nav>
+        <div className="main-menu" >
+            <nav >
                 <NavLink to="/" ><img className="img-menu home" src="./assets/img/home.png" /></NavLink>
                 <NavLink to="/mail"><img className="img-menu mail" src="./assets/img/mail.png" /></NavLink>
                 <NavLink to="/note"><img className="img-menu note" src="./assets/img/keep.png" /></NavLink>
